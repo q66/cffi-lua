fc463e40aed35a492b2f5575bfabfa5cef4f1fc1
Daniel Kolesa <daniel@octaforge.org>
2020-02-23 01:12:47 +0100

    initial support for library handles

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <dlfcn.h>
 
 #include <lua.hpp>
 
@@ -17,8 +18,27 @@ static const luaL_Reg cffi_lib[] = {
     {NULL, NULL}
 };
 
+static int cffi_free_handle(lua_State *L) {
+    void **c_ud = static_cast<void **>(lua_touserdata(L, 1));
+    dlclose(*c_ud);
+    return 0;
+}
+
+static void cffi_setup_lib_handle_meta(lua_State *L) {
+    lua_pushcfunction(L, cffi_free_handle);
+    lua_setfield(L, -2, "__gc");
+}
+
 extern "C" int luaopen_cffi(lua_State *L) {
     luaL_newlib(L, cffi_lib);
+
+    void **c_ud = static_cast<void **>(lua_newuserdata(L, sizeof(void *)));
+    *c_ud = dlopen(nullptr, RTLD_NOW);
+    if (luaL_newmetatable(L, "cffi_lib_handle")) {
+        cffi_setup_lib_handle_meta(L);
+        lua_setmetatable(L, -2);
+    }
+    lua_setfield(L, -2, "C");
 
     return 1;
 }
--- test.lua
+++ test.lua
@@ -3,3 +3,5 @@ local ffi = require("cffi")
 ffi.cdef [[
     long int strtol(const char *str, char **endptr, int base);
 ]]
+
+print(ffi.C)
