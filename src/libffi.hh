559c978effcf4e9ee366a41abb7e2b1e3ca2d300
Daniel Kolesa <daniel@octaforge.org>
2021-03-08 22:12:48 +0100

    more meson simplifications

--- src/libffi.hh
+++ src/libffi.hh
@@ -22,11 +22,7 @@
 #pragma clang diagnostic ignored "-Wlanguage-extension-token"
 #endif
 
-#ifdef HAVE_FFI_FFI_H
-#include <ffi/ffi.h>
-#else
 #include <ffi.h>
-#endif
 
 #if defined(FFI_DIAGNOSTIC_PRAGMA_CLANG)
 #pragma clang diagnostic pop
